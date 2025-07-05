2c6c0a31602cd389c625d2b0e8c8c56dd8d5c29e
Vajrala Venkateswarlu <59252985+venkateswarluvajrala@users.noreply.github.com>
2025-07-05 14:23:34 +0530

    Restructure project (#8)

--- src/tests/test_notebook.py
+++ src/tests/test_notebook.py
@@ -2,23 +2,36 @@ from typing import cast
 from unittest.mock import patch, MagicMock
 import kopf
 import kubernetes.client
+import pytest
 
-from src.mlpad.notebook_handler import create_notebook
+from src.main.mlpad.notebook.handler import create_notebook
+
+
+@pytest.fixture()
+def notebook_spec():
+    storage_size = 10
+    spec = cast(
+        kopf.Spec,
+        {
+            "storageSize": 10,
+            "image": "code-server",
+            "containerSize": "small",
+        },
+    )
+    data = {
+        "name": "valid-notebook",
+        "namespace": "mlpad",
+        "storage_size": storage_size,
+        "spec": spec,
+    }
+    return data
 
 
 class TestNotebook:
-    def test_should_create_new_notebook_with_valid_values(self):
-        name = "valid-notebook"
-        namespace = "mlpad"
-        storage_size = 10
-        spec = cast(
-            kopf.Spec,
-            {
-                "storageSize": storage_size,
-                "image": "code-server",
-                "containerSize": "small",
-            },
-        )
+    def test_should_create_new_notebook_with_valid_values(self, notebook_spec):
+        name = notebook_spec.get("name")
+        namespace = notebook_spec.get("namespace")
+        storage_size = notebook_spec.get("storage_size")
         object_meta = {
             "metadata": kubernetes.client.V1ObjectMeta(
                 annotations={"storageReadable": f"{storage_size}Gi"},
@@ -27,13 +40,15 @@ class TestNotebook:
         }
 
         with patch(
-            "src.mlpad.notebook_handler.client.CustomObjectsApi"
+                "src.main.mlpad.notebook.handler.client.CustomObjectsApi"
         ) as mock_custom_objects_api:
             mock_kubernetes_client = MagicMock()
             mock_custom_objects_api.return_value = mock_kubernetes_client
             mock_kubernetes_client.patch_namespaced_custom_object.return_value = None
 
-            create_notebook(name=name, namespace=namespace, spec=spec)
+            create_notebook(
+                name=name, namespace=namespace, spec=notebook_spec.get("spec")
+            )
 
             mock_kubernetes_client.patch_namespaced_custom_object.assert_called_once_with(
                 group="mlpad.venkateswarluvajrala.com",
